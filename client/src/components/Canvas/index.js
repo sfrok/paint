e98893ade557adb5ef180d5b4d4593ceb5888695
Sergey <sfrok99@gmail.com>
2021-01-09 16:54:55 +0200

    feat(modal): added the modal window to get connecting for users

--- client/src/components/Canvas/index.js
+++ client/src/components/Canvas/index.js
@@ -1,6 +1,7 @@
 // Core
 import React, { useRef, useEffect } from "react";
 import { observer } from "mobx-react-lite";
+import { Button, FormControl, InputGroup, Modal } from "react-bootstrap";
 
 // GlobalStyle
 import { Container, Row, Column } from "../../shared";
@@ -26,10 +27,34 @@ export const Canvas = observer(() => {
     canvasState.pushToUndo(canvasState.canvas.toDataURL());
   };
 
+  const connectionHandler = () => {};
+
   return (
     <Container>
       <Row>
         <Column width={12} contentPosition="center">
+          <Modal
+            show={true}
+            onHide={() => {}}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+          >
+            <Modal.Header closeButton>
+              <Modal.Title id="contained-modal-title-vcenter">
+                Enter username:
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <InputGroup>
+                <FormControl placeholder="Username" aria-label="Username" />
+              </InputGroup>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={() => connectionHandler()}>Sign In</Button>
+            </Modal.Footer>
+          </Modal>
+          :
           <Area
             ref={canvasRef}
             width={800}
